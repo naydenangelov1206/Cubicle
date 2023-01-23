5b15542e5b7d78847c8c5efc6e25751112f45d29
naydenangelov1206 <naydenangelov1206@gmail.com>
2023-01-23 15:21:41 +0200

    views are working

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ const setupViewEngine = require("./config/viewEngine");
 const app = express();
 setupViewEngine(app);
 
+//? It looks if they are any files in the public folder and reads them
+app.use(express.static("./src/public"));
+
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(config.PORT, () =>
